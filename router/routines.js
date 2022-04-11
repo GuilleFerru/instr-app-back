d5871629f0fadf0fbfe9aedf41fc0aa3100cad84
GuilleFerru <guillelf@gmail.com>
2022-04-10 21:14:15 -0300

    commit to heroku

--- router/routines.js
+++ router/routines.js
@@ -1,13 +1,8 @@
 import express from 'express';
-import timeout from "connect-timeout";
 import { ControllerRoutine } from '../controllers/routines.js';
 import { userExtractor } from '../utils/userExtractor.js';
 
 const router = express.Router();
-
-const haltOnTimedoutMiddleware = () => {
-    if (!req.timedout) next()
-}
 
 export class RouterRoutine {
     constructor() {
@@ -17,7 +12,7 @@ export class RouterRoutine {
     start() {
         router.post('/create', userExtractor, this.controllerRoutine.createRoutine);
         router.get('/get', userExtractor, this.controllerRoutine.getRoutine);
-        router.get('/getAllRoutines/:date', userExtractor, timeout('50s'), haltOnTimedoutMiddleware, this.controllerRoutine.getAllRoutine);
+        router.get('/getAllRoutines/:date', userExtractor, this.controllerRoutine.getAllRoutine);
         router.put('/update', userExtractor, this.controllerRoutine.updateRoutineScheduleByCompleteTask);
         router.put('/updateOt', userExtractor, this.controllerRoutine.updateRoutineScheduleOT);
         return router;
